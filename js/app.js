50114de55e7fa5ec6de86dd4e00ef775854e6e9e
Abner Castro <abnerfcastro@gmail.com>
2018-08-05 02:04:46 -0300

    feat: Build deck of card elements on the page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,29 @@
+const cardTypes = [
+    'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
+    'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'
+];
+
+const cardIcons = [...cardTypes, ...cardTypes];
+
 const deckElement = document.querySelector('.deck');
+const cardElements = document.getElementsByClassName('card');
 
 deckElement.addEventListener('click', function (event) {
     const element = event.target;
     element.classList.toggle('open');
     element.classList.toggle('show');
 });
+
+function buildCardElements() {
+    for (let i = 0; i < cardElements.length; i++) {
+        const iTag = document.createElement('i');
+        iTag.className = `fa ${cardIcons[i]}`;
+        cardElements[i].appendChild(iTag);
+    }
+}
+
+function init() {
+    buildCardElements();
+}
+
+init();

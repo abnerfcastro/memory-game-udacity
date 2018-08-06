9a975668ea668d17cd9d1190e06182b82397f0c1
Abner Castro <=>
2018-08-06 16:47:47 -0300

    refactor: Rewrite functions and better code organization

--- js/app.js
+++ js/app.js
@@ -1,65 +1,78 @@
-const cardTypes = [
+const icons = [
     'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
     'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'
 ];
 
-const cardIcons = [...cardTypes, ...cardTypes];
+const cards = [...icons, ...icons];
 
 const deckElement = document.querySelector('.deck');
-const cardElements = document.getElementsByClassName('card');
+// const cardElements = document.getElementsByClassName('card');
+
+const cardElementsList = getCardElementsArray();
 
 const openCards = [];
-
-let arrayOfCards = [];
 
 deckElement.addEventListener('click', function (event) {
     const element = event.target;
 
-    reveal(element);
+    if (!isRevealed(element)) {
+        reveal(element);
 
-    let index = arrayOfCards.indexOf(element);
-    openCards.push(index);
+        let index = cardElementsList.indexOf(element);
+        openCards.push(index);
 
+        checkForMatch();
+    }
+});
+
+function checkForMatch() {
     if (openCards.length === 2) {
         let idx1 = openCards[0];
         let idx2 = openCards[1];
 
-        let card1 = arrayOfCards[idx1];
-        let card2 = arrayOfCards[idx2];
+        let card1 = cardElementsList[idx1];
+        let card2 = cardElementsList[idx2];
 
-        if (cardIcons[idx1] === cardIcons[idx2]) {
-            console.log('Match!');
+        if (cards[idx1] === cards[idx2]) {
             match(card1, card2);
         } else {
-            console.log('Not a match');
-            hide(card1);
-            hide(card2);
+            hide(card1, card2);
         }
-
-        openCards.splice(0, 2);
-    }
-});
-
-function reveal(card) {
-    card.classList.add('open', 'show');
-}
-
-function match(card1, card2) {
-    if (card1 && card2) {
-        card1.classList.add('match');
-        card2.classList.add('match');
     }
 }
 
-function hide(card) {
-    card.classList.remove('open', 'show');
+function isRevealed(card) {
+    return card.classList.contains('open');
+}
+
+function reveal() {
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] && arguments[i].classList) {
+            arguments[i].classList.add('open', 'show');
+        }
+    }
+}
+
+function match(firstCard, secondCard) {
+    if (firstCard && firstCard.classList && secondCard && secondCard.classList) {
+        firstCard.classList.add('match');
+        secondCard.classList.add('match');
+    }
+}
+
+function hide() {
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] && arguments[i].classList) {
+            arguments[i].classList.remove('open', 'show');
+        }
+    }
 }
 
 function buildCardElements() {
-    for (let i = 0; i < cardElements.length; i++) {
+    for (let i = 0; i < cardElementsList.length; i++) {
         const iTag = document.createElement('i');
-        iTag.className = `fa ${cardIcons[i]}`;
-        cardElements[i].appendChild(iTag);
+        iTag.className = `fa ${cards[i]}`;
+        cardElementsList[i].appendChild(iTag);
     }
 }
 
@@ -85,14 +98,17 @@ function shuffle(array) {
     return array;
 }
 
+function getCardElementsArray() {
+    let cardsHtmlCollection = document.getElementsByClassName('card');
+    return cardsHtmlCollection ? Array.prototype.slice.call(cardsHtmlCollection) : null;
+}
+
 /**
  * @description Initialize the game
  */
 function init() {
-    shuffle(cardIcons);
+    shuffle(cards);
     buildCardElements();
-
-    arrayOfCards = Array.prototype.slice.call(cardElements);
 }
 
 init();

22d92b9a4e7710aee8d6e7b6226c6820d264771c
Abner Castro <abnerfcastro@gmail.com>
2018-08-05 21:59:22 -0300

    feat: Implement initial matching logic

--- js/app.js
+++ js/app.js
@@ -8,11 +8,52 @@ const cardIcons = [...cardTypes, ...cardTypes];
 const deckElement = document.querySelector('.deck');
 const cardElements = document.getElementsByClassName('card');
 
+const openCards = [];
+
+let arrayOfCards = [];
+
 deckElement.addEventListener('click', function (event) {
     const element = event.target;
-    element.classList.toggle('open');
-    element.classList.toggle('show');
+
+    reveal(element);
+
+    let index = arrayOfCards.indexOf(element);
+    openCards.push(index);
+
+    if (openCards.length === 2) {
+        let idx1 = openCards[0];
+        let idx2 = openCards[1];
+
+        let card1 = arrayOfCards[idx1];
+        let card2 = arrayOfCards[idx2];
+
+        if (cardIcons[idx1] === cardIcons[idx2]) {
+            console.log('Match!');
+            match(card1, card2);
+        } else {
+            console.log('Not a match');
+            hide(card1);
+            hide(card2);
+        }
+
+        openCards.splice(0, 2);
+    }
 });
+
+function reveal(card) {
+    card.classList.add('open', 'show');
+}
+
+function match(card1, card2) {
+    if (card1 && card2) {
+        card1.classList.add('match');
+        card2.classList.add('match');
+    }
+}
+
+function hide(card) {
+    card.classList.remove('open', 'show');
+}
 
 function buildCardElements() {
     for (let i = 0; i < cardElements.length; i++) {
@@ -22,14 +63,14 @@ function buildCardElements() {
     }
 }
 
-/*
- * Display the cards on the page
+/**
+ * @description Display the cards on the page
+ * Shuffle function from http://stackoverflow.com/a/2450976
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
+ * @param {Array} array - The array to be shuffled
  */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -44,8 +85,14 @@ function shuffle(array) {
     return array;
 }
 
+/**
+ * @description Initialize the game
+ */
 function init() {
+    shuffle(cardIcons);
     buildCardElements();
+
+    arrayOfCards = Array.prototype.slice.call(cardElements);
 }
 
 init();
